52618d3c9381afc70f5f0fba214d29bd70381826
xuerivera <xuerivera@gmail.com>
2024-05-16 17:26:47 -0400

    bypass jekyll, added workflows action

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
-
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://github.com/xuerivera',
-  base: '/ready2eat',
-  integrations: [tailwind()]
+  site: 'https://xuerivera.github.io/ready2eat',
+  base: '/ready2eat/',
+  integrations: [tailwind()],
+  build: {
+    outDir: 'dist'
+  }
 });
