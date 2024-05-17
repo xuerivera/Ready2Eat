b6e6b328094fefb37e253e255a337012781d3be1
xuerivera <xuerivera@gmail.com>
2024-05-16 20:25:33 -0400

    deleted all setup for github.io

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,6 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://xuerivera.github.io/ready2eat',
-  base: '/ready2eat/',
   integrations: [tailwind()],
-  build: {
-    outDir: 'dist'
-  }
+ 
 });
